9cc6bfc6d6c4afa05732149d0323cc9f0624f036
JakeBrowning90 <jacobbrowning90@gmail.com>
2026-03-19 14:33:14 -0400

    Update game logic to use UI buttons and display

--- script.js
+++ script.js
@@ -27,38 +27,80 @@ function getHumanChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
+  humanMove.textContent = "You play " + humanChoice;
+  cpuMove.textContent = "CPU plays " + computerChoice;
   if (
     (computerChoice == "rock" && humanChoice == "scissors") ||
     (computerChoice == "scissors" && humanChoice == "paper") ||
     (computerChoice == "paper" && humanChoice == "rock")
   ) {
-    console.log("You lost! " + computerChoice + " beats " + humanChoice);
+    outcomeHeading.textContent =
+      "You lost! " + computerChoice + " beats " + humanChoice;
     return "loss";
   } else if (
     (humanChoice == "rock" && computerChoice == "scissors") ||
     (humanChoice == "scissors" && computerChoice == "paper") ||
     (humanChoice == "paper" && computerChoice == "rock")
   ) {
-    console.log("You won! " + humanChoice + " beats " + computerChoice);
+    outcomeHeading.textContent =
+      "You won! " + humanChoice + " beats " + computerChoice;
     return "win";
   } else {
-    console.log("You tied! " + humanChoice + " vs. " + computerChoice);
+    outcomeHeading.textContent =
+      "You tied! " + humanChoice + " vs. " + computerChoice;
     return "tie";
   }
 }
 
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
-  do {
-    let outcome = playRound(getComputerChoice(), getHumanChoice());
-    if (outcome == "loss") {
-      computerScore++;
-    } else if (outcome == "win") {
-      humanScore++;
-    }
-    console.log("You: " + humanScore + " / Computer: " + computerScore);
-  } while (computerScore < 5 && humanScore < 5);
+function updateScore(result) {
+  if (result == "win") {
+    humanScore++;
+    humanScoreSpan.textContent = humanScore;
+  } else if (result == "loss") {
+    computerScore++;
+    cpuScoreSpan.textContent = computerScore;
+  }
+  checkEndgame();
 }
 
-playGame();
+function checkEndgame() {
+  if (computerScore == 5 || humanScore == 5) {
+    // Remove buttons
+    rockButton.remove();
+    paperButton.remove();
+    scissorsButton.remove();
+
+    if (humanScore == 5) {
+      outcomeHeading.textContent = "Game over, you won!";
+    } else if (computerScore == 5) {
+      outcomeHeading.textContent = "Game over, you lost!";
+    }
+  }
+}
+
+const rockButton = document.getElementById("rockButton");
+rockButton.addEventListener("click", () => {
+  updateScore(playRound(getComputerChoice(), "rock"));
+});
+
+const paperButton = document.getElementById("paperButton");
+paperButton.addEventListener("click", () => {
+  updateScore(playRound(getComputerChoice(), "paper"));
+});
+
+const scissorsButton = document.getElementById("scissorsButton");
+scissorsButton.addEventListener("click", () => {
+  updateScore(playRound(getComputerChoice(), "scissors"));
+});
+
+let computerScore = 0;
+let humanScore = 0;
+
+const humanMove = document.getElementById("humanMove");
+const cpuMove = document.getElementById("cpuMove");
+const outcomeHeading = document.getElementById("outcomeHeading");
+
+const humanScoreSpan = document.getElementById("humanScoreSpan");
+humanScoreSpan.textContent = humanScore;
+const cpuScoreSpan = document.getElementById("cpuScoreSpan");
+cpuScoreSpan.textContent = computerScore;
